542f70a99046b62bdd35a357c5d8e55715e8cc9d
aimable <aganzerizen@gmail.com>
2024-05-05 17:57:48 +0200

    5 May 24

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+import daisyui from "daisyui"
+
 export default {
   content: ["./index.html", 
   "./src/**/*.{js,ts,jsx,tsx}"],
@@ -6,6 +9,9 @@ export default {
     extend: {},
   },
   //...
-  plugins: [require("daisyui")],
+  // plugins: [require("daisyui")],
+  plugins: [
+    daisyui,
+  ],
 }
 
